7b066d3f348fe2ed51994a0e55b32f620c6f229d
hiimzimmy <holcmannsimonas@gmail.com>
2022-04-07 10:59:35 +0300

    fix(analytics): remove legacy analytics

--- src/utils.js
+++ src/utils.js
@@ -1,26 +1,10 @@
 function IdIsValid(selector) {
-  return $(`#${selector}`).length > 0;
+    return $(`#${selector}`).length > 0;
 }
 
 function TryToGetTitle(selector) {
-  return $(`#${selector}`).attr('title') !== undefined && $(`#${selector}`).attr('title') !== false ? $(`#${selector}`).attr('title') : selector;
+    return $(`#${selector}`).attr('title') !== undefined && $(`#${selector}`).attr('title') !== false ? $(`#${selector}`).attr('title') : selector;
 }
-
-function HotspotAnalytics(scene) {
-  SendGAEvent("moveTo", "hotspot", scene);
-}
-
-function SendGAEvent(name, event_category, event_label) {
-  console.log(event_label);
-  if (typeof gtag === "function") gtag("event", name, {
-    event_category: event_category,
-    event_label: event_label,
-  });
-  else console.warn("gtag does not exist.");
-}
-
 
 window.IdIsValid = IdIsValid;
 window.TryToGetTitle = TryToGetTitle;
-window.HotspotAnalytics = HotspotAnalytics;
-window.SendGAEvent = SendGAEvent;
